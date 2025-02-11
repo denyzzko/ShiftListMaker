b9864e1eb3351ff8bba2cafd8c51e1ff2a29f6d8
denyzzko <126917003+denyzzko@users.noreply.github.com>
2025-02-11 17:47:08 +0100

    persistent table storage system & top menu bar

--- ShiftList.py
+++ ShiftList.py
@@ -1,11 +1,15 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QFormLayout, QTableWidget, 
-    QTableWidgetItem, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel, QHBoxLayout
+    QTableWidgetItem, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel, QHBoxLayout,
+    QMenuBar, QMenu
 )
-from PyQt6.QtGui import QIntValidator, QFont
-from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIntValidator, QFont, QAction
+from PyQt6.QtCore import Qt, QCoreApplication
 import sys
+import json
+
+JSON_FILE = "shifts.json"
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -18,10 +22,44 @@ class MyWindow(QMainWindow):
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
 
-        # layouts
+        # Layouts
         self.main_layout = QVBoxLayout()
         self.form_layout = QVBoxLayout()
+        self.menu_layout = QHBoxLayout()
 
+        # Top menu bar (NOT COMPLETED YET -- TODO REST OF MENU SECTIONS AND SHORTCUTS)
+        self.menubar = QMenuBar()
+        _translate = QCoreApplication.translate
+        
+        self.menuFile = self.menubar.addMenu("File")
+        self.menuEdit = self.menubar.addMenu("Edit")
+        self.menuView = self.menubar.addMenu("View")
+        self.menuHelp = self.menubar.addMenu("Help")
+
+        self.actionNew = QAction("New", self)
+        self.actionNew.triggered.connect(self.createTable)
+        self.menuFile.addAction(self.actionNew)
+        self.actionNew.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        
+        self.menuFile.addAction("Open")
+        
+        self.actionSave = QAction("Save", self)
+        self.actionSave.triggered.connect(self.storeToJSON)
+        self.menuFile.addAction(self.actionSave)
+        self.actionSave.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        
+        self.actionLoad = QAction("Load", self)
+        self.actionLoad.triggered.connect(self.loadFromJSON)
+        self.menuFile.addAction(self.actionLoad)
+        self.actionLoad.setShortcut(_translate("MainWindow", "Ctrl+L"))
+
+        self.menuFile.addSeparator()
+        
+        self.menuFile.addAction("Quit")
+
+        # Add menu bar to the menu layout
+        self.menu_layout.addWidget(self.menubar)
+  
         # Error label
         self.error_label = QLabel("")
 
@@ -52,14 +90,15 @@ class MyWindow(QMainWindow):
         self.generateButton = QPushButton("Doplniť zmeny")
         self.generateButton.clicked.connect(self.generateShifts)
 
-        # adding input fields to the form layout
+        # Adding input fields to the form layout
         self.form_layout.addWidget(self.error_label)
         self.form_layout.addWidget(self.label_num_workers)
         self.form_layout.addWidget(self.input_num_workers)
         self.form_layout.addWidget(self.label_num_days)
         self.form_layout.addWidget(self.input_num_days)
 
-        # adding everything to the main layout
+        # Adding everything to the main layout
+        self.main_layout.addLayout(self.menu_layout)
         self.main_layout.addLayout(self.form_layout)
         self.main_layout.addWidget(self.createButton)
         self.main_layout.addWidget(self.tableWidget)
@@ -67,6 +106,7 @@ class MyWindow(QMainWindow):
         
         self.centralWidget.setLayout(self.main_layout)
 
+    # Function to draw table on screen
     def createTable(self):
         try:
             num_workers = int(self.input_num_workers.text())
@@ -75,27 +115,85 @@ class MyWindow(QMainWindow):
             if num_workers <= 0 or num_days <= 0:
                 raise ValueError
             
-            # set table size
+            # Set table size
             self.tableWidget.setRowCount(num_workers)
             self.tableWidget.setColumnCount(num_days + 1)
 
-            # set headers
+            # Set headers
             column_headers = ["Meno"]
             for i in range(num_days):
                 column_headers.append(str(i + 1))
             self.tableWidget.setHorizontalHeaderLabels(column_headers)
 
+            # Adjust column widths
+            self.tableWidget.setColumnWidth(0, 130)  # Wider "Meno" column (150px)
+            for col in range(1, num_days + 1):
+                self.tableWidget.setColumnWidth(col, 30)  # Narrower shift columns (50px each)
+
             self.error_label.setText("Tabuľka úspešne vytvorená :)")
             
         except ValueError:
             self.error_label.setText("Zadané číslo je nesprávne!")
-
-    def generateShifts(self):
-        self.label.setText("You pressed the button !")
-        self.update()
     
-    def update(self):
-        self.label.adjustSize()
+    # Function to store table content to JSON file
+    def storeToJSON(self):
+        try:
+            # JSON object with data to be stored
+            json_object = {
+                "num_days":self.tableWidget.columnCount() - 1,
+                "num_workers":self.tableWidget.rowCount(),
+                "workers":[]
+            }
+
+            # Populate JSON object "worker" section from table
+            for row in range(self.tableWidget.rowCount()):
+                worker_name = self.tableWidget.item(row, 0)
+                shifts = []
+                for col in range(1, self.tableWidget.columnCount()):
+                    shift_item = (self.tableWidget.item(row,col))
+                    shifts.append(shift_item.text() if shift_item else "")
+                json_object["workers"].append(
+                    {
+                        "name":worker_name.text() if worker_name else "",
+                        "shifts":shifts
+                    }
+                )
+            
+            # Open JSON file and write data
+            with open(JSON_FILE, "w", encoding="utf-8") as jsonfile:
+                json.dump(json_object, jsonfile, indent=4, ensure_ascii=False)
+            
+            self.error_label.setText("Tabulka uspesne ulozena :)")
+            
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.error_label.setText("Chyba pri ukladani obsahu z tabulky do suboru!")
+    
+    # Function to load table content from stored JSON file
+    def loadFromJSON(self):
+        try:
+            # Open and get data from JSON file
+            with open(JSON_FILE, "r", encoding="utf-8") as jsonfile:
+                data = json.load(jsonfile)
+
+            # Insert loaded values to input fields and create table
+            self.input_num_days.setText(str(data["num_days"]))   
+            self.input_num_workers.setText(str(data["num_workers"]))
+            self.createTable()
+
+            # Populate table with loaded json data
+            for row, worker in enumerate(data["workers"]):
+                self.tableWidget.setItem(row, 0, QTableWidgetItem(worker["name"]))
+                for col, shift in enumerate(worker["shifts"], start=1):
+                    self.tableWidget.setItem(row, col, QTableWidgetItem(shift))
+            
+            self.error_label.setText("Tabulka uspesne nacitana :)")
+            
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.error_label.setText("Chyba pri nacitani obsahu ulozenej tabulky!")
+
+    # Function to automatically fill in table with shifts
+    def generateShifts(self):
+        self.error_label.setText("Tato funkcia este nie je implementovana...")
 
 def window():
     app = QApplication(sys.argv)
