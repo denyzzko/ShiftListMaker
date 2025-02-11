fda85a4903c57a464b2079be04041c064a447573
denyzzko <126917003+denyzzko@users.noreply.github.com>
2025-02-11 11:26:42 +0100

    gui version 0.5

--- ShiftList.py
+++ ShiftList.py
@@ -1,11 +1,16 @@
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QFormLayout, QTableWidget, 
+    QTableWidgetItem, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel, QHBoxLayout
+)
+from PyQt6.QtGui import QIntValidator, QFont
+from PyQt6.QtCore import Qt
 import sys
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        self.setGeometry(200,200,300,300)
+        self.setGeometry(200,200,1200,600)
         self.setWindowTitle("Shift List Maker")
         self.initUI()
 
@@ -13,41 +18,80 @@ class MyWindow(QMainWindow):
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
 
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("This is Label")
-        self.label.move(50,50)
+        # layouts
+        self.main_layout = QVBoxLayout()
+        self.form_layout = QVBoxLayout()
 
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Generate")
-        self.b1.clicked.connect(self.button_clicked)
+        # Error label
+        self.error_label = QLabel("")
 
-        self.createTable()
-
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.tableWidget)
-        self.layout.addWidget(self.b1)
+        # Input field for number of workers
+        self.label_num_workers = QLabel("Počet pracovníkov:")
+        self.input_num_workers = QLineEdit()
+        self.input_num_workers.setValidator(QIntValidator())
+        self.input_num_workers.setMaxLength(3)
+        self.input_num_workers.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.input_num_workers.setFont(QFont("Arial", 20))
         
-        self.centralWidget.setLayout(self.layout)
+        # Input field for number of days
+        self.label_num_days = QLabel("Počet dní v mesiaci:")
+        self.input_num_days = QLineEdit()
+        self.input_num_days.setValidator(QIntValidator())
+        self.input_num_days.setMaxLength(3)
+        self.input_num_days.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.input_num_days.setFont(QFont("Arial", 20))
+
+        # Button to draw table
+        self.createButton = QPushButton("Vytvoriť tabuľku")
+        self.createButton.clicked.connect(self.createTable)
+
+        # Table widget (empty initially)
+        self.tableWidget = QTableWidget()
+
+        # Button to fill table with shifts
+        self.generateButton = QPushButton("Doplniť zmeny")
+        self.generateButton.clicked.connect(self.generateShifts)
+
+        # adding input fields to the form layout
+        self.form_layout.addWidget(self.error_label)
+        self.form_layout.addWidget(self.label_num_workers)
+        self.form_layout.addWidget(self.input_num_workers)
+        self.form_layout.addWidget(self.label_num_days)
+        self.form_layout.addWidget(self.input_num_days)
+
+        # adding everything to the main layout
+        self.main_layout.addLayout(self.form_layout)
+        self.main_layout.addWidget(self.createButton)
+        self.main_layout.addWidget(self.tableWidget)
+        self.main_layout.addWidget(self.generateButton)
+        
+        self.centralWidget.setLayout(self.main_layout)
 
     def createTable(self):
-        self.tableWidget = QTableWidget()
-        #row count
-        self.tableWidget.setRowCount(4)
-        #column count
-        self.tableWidget.setColumnCount(2)
+        try:
+            num_workers = int(self.input_num_workers.text())
+            num_days = int(self.input_num_days.text())
 
-        self.tableWidget.setItem(0,0, QTableWidgetItem("Name")) 
-        self.tableWidget.setItem(0,1, QTableWidgetItem("City")) 
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Aloysius")) 
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Indore")) 
-        self.tableWidget.setItem(2,0, QTableWidgetItem("Alan")) 
-        self.tableWidget.setItem(2,1, QTableWidgetItem("Bhopal")) 
-        self.tableWidget.setItem(3,0, QTableWidgetItem("Arnavi")) 
-        self.tableWidget.setItem(3,1, QTableWidgetItem("Mandsaur"))
+            if num_workers <= 0 or num_days <= 0:
+                raise ValueError
+            
+            # set table size
+            self.tableWidget.setRowCount(num_workers)
+            self.tableWidget.setColumnCount(num_days + 1)
 
-    def button_clicked(self):
-        self.label.setText("You pressed the button")
+            # set headers
+            column_headers = ["Meno"]
+            for i in range(num_days):
+                column_headers.append(str(i + 1))
+            self.tableWidget.setHorizontalHeaderLabels(column_headers)
+
+            self.error_label.setText("Tabuľka úspešne vytvorená :)")
+            
+        except ValueError:
+            self.error_label.setText("Zadané číslo je nesprávne!")
+
+    def generateShifts(self):
+        self.label.setText("You pressed the button !")
         self.update()
     
     def update(self):
